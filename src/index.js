7f832fe210afc8cb49ac020eb901c2a367be6d32
Daniil <beltyukovdanil@gmail.com>
2019-05-13 14:35:54 +0300

    refactor: moved express app to class

--- src/index.js
+++ src/index.js
@@ -1,30 +1,58 @@
-const { ApolloServer, gql } = require("apollo-server-express");
-const express = require("express");
-const mongoose = require("mongoose");
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
+const mongoose = require('mongoose');
 
-const { resolvers } = require("./resolvers");
-const { typeDefs } = require("./typeDefs");
+const { resolvers } = require('./resolvers');
+const { typeDefs } = require('./typeDefs');
 
-const startServer = async () => {
-  const app = express();
-
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers
-  });
-
-  server.applyMiddleware({ app });
-
-  await mongoose.connect(
-    process.env.MONGO_URL || "mongodb://localhost:27017/",
-    {
-      useNewUrlParser: true
-    }
-  );
-
-  app.listen({ port: 4000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-  );
-};
+const startServer = async () => {};
 
 startServer();
+
+/**
+ *
+ *
+ * @class HawkAPI
+ */
+class HawkAPI {
+  /**
+   *Creates an instance of HawkAPI.
+   * @memberof HawkAPI
+   */
+  constructor() {
+    this.config = {
+      port: process.env.PORT || 4000,
+      mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/'
+    };
+
+    this.app = express();
+
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers
+    });
+
+    server.applyMiddleware({ app: this.app });
+  }
+
+  /**
+   *
+   *
+   * @memberof HawkAPI
+   */
+  async start() {
+    await mongoose.connect(this.config.mongoURL, {
+      useNewUrlParser: true
+    });
+
+    app.listen({ port: this.config.port }, () =>
+      console.log(
+        `🚀 Server ready at :${this.config.port}${server.graphqlPath}`
+      )
+    );
+  }
+}
+
+module.exports = {
+  HawkAPI
+};
