2986a1634737c0c1e6c6ffbc3808a0a8f9a61363
Nikita Melnikov <nikmel2803@gmail.com>
2019-05-24 23:10:52 +0300

    split resolvers

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,41 +1,13 @@
-const User = require('../models/user');
+const user = require('./user');
+const { merge } = require('lodash');
 
-const { GraphQLString } = require('graphql');
-const { AuthenticationError } = require('apollo-server-express/dist/index');
-
-module.exports = {
-  Token: {
-    GraphQLString,
-    name: 'Token'
-  },
+const indexResolver = {
   Query: {
-    health: () => 'ok',
-
-    async me(_, __, { user }) {
-      if (!user.userId) {
-        return null;
-      }
-
-      return User.findById(user.userId);
-    }
-  },
-  Mutation: {
-    async signUp(_, { email }) {
-      const user = await User.create(email);
-
-      console.log(`New user: email: ${user.email}, password: ${user.generatedPassword}`);
-
-      return user.generateJWT();
-    },
-
-    async login(_, { email, password }) {
-      const user = await User.findOne({ email });
-
-      if (!user || !await user.comparePassword(password)) {
-        throw new AuthenticationError('Wrong email or password');
-      }
-
-      return user.generateJWT();
-    }
+    health: () => 'ok'
   }
 };
+
+module.exports = merge(
+  indexResolver,
+  user
+);
---
+++ src/resolvers/user.js
@@ -0,0 +1,38 @@
+const { GraphQLString } = require('graphql');
+const { AuthenticationError } = require('apollo-server-express');
+const User = require('../models/user');
+
+module.exports = {
+  Token: {
+    GraphQLString,
+    name: 'Token'
+  },
+  Query: {
+    async me(_, __, { user }) {
+      if (!user.userId) {
+        return null;
+      }
+
+      return User.findById(user.userId);
+    }
+  },
+  Mutation: {
+    async signUp(_, { email }) {
+      const user = await User.create(email);
+
+      console.log(`New user: email: ${user.email}, password: ${user.generatedPassword}`);
+
+      return user.generateJWT();
+    },
+
+    async login(_, { email, password }) {
+      const user = await User.findOne({ email });
+
+      if (!user || !await user.comparePassword(password)) {
+        throw new AuthenticationError('Wrong email or password');
+      }
+
+      return user.generateJWT();
+    }
+  }
+};
