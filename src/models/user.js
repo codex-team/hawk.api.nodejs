79e135166e9bfc0d0ff3d247f4681112641d7fab
Nikita Melnikov <nikmel2803@gmail.com>
2019-05-24 14:40:56 +0300

    create User.create method

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const argon2 = require('argon2');
+const crypto = require('crypto');
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   email: {
     type: String,
     unique: true
@@ -17,7 +19,23 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-const User = mongoose.model('User', UserSchema);
+/**
+ * Creates new user in DB
+ * @param {String} email - user email
+ * @returns {Promise} - created user
+ */
+userSchema.statics.create = async function (email) {
+  const generatedPassword = crypto.randomBytes(8).toString('hex');
+  const hashedPassword = await argon2.hash(generatedPassword);
+  const user = new this({ email, password: hashedPassword });
+
+  return {
+    ...await user.save(),
+    generatedPassword
+  };
+};
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = {
   User
