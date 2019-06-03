8469f304f5def742847464cad2ffc1dfc64d24d1
Nikita Melnikov <nikmel2803@gmail.com>
2019-06-03 20:20:58 +0300

    remove unnecessary code from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true,
-    node: true
-  },
-  extends: ['codex'],
-  parserOptions: {
-    ecmaVersion: 2018
-  },
-  rules: {}
+  extends: ['codex']
 };
