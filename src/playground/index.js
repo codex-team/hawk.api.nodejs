763a0a5b1babafa72e929018c4bbf82b50213ff4
Nikita Melnikov <nikmel2803@gmail.com>
2019-06-22 15:05:43 +0300

    add headers and variables to the tabs

--- src/playground/index.js
+++ src/playground/index.js
@@ -1,11 +1,22 @@
 const fs = require('fs');
 
+const headers = {
+  'authorization': 'Bearer <access token>'
+};
+
+const variables = {
+  accessToken: 'lol'
+};
+
 const tabs = fs.readdirSync(__dirname)
   .filter(name => name.endsWith('.graphql'))
   .map(fileName => {
     return {
+      name: fileName.slice(0, -8),
       endpoint: `http://localhost:${process.env.PORT}/graphql`,
-      query: fs.readFileSync(`${__dirname}/${fileName}`).toString()
+      query: fs.readFileSync(`${__dirname}/${fileName}`).toString(),
+      variables: JSON.stringify(variables),
+      headers
     };
   });
 
