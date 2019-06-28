41a6a3e1dfe35c07f374f98dbae9f17aa8ae4e96
Nikita Melnikov <nikmel2803@gmail.com>
2019-06-28 14:13:15 +0300

    create yandex email provider

---
+++ src/email/providers/YandexProvider.js
@@ -0,0 +1,51 @@
+const nodemailer = require('nodemailer');
+const EmailProvider = require('./EmailProvider');
+
+/**
+ * Class representing Yandex email service provider
+ */
+class YandexProvider extends EmailProvider {
+  /**
+   * Creates provider instance
+   */
+  constructor() {
+    super();
+
+    this.config = {
+      host: process.env.YANDEX_MAIL_HOST,
+      port: process.env.YANDEX_MAIL_PORT,
+      secure: true,
+      auth: {
+        user: process.env.YANDEX_MAIL_USERNAME,
+        pass: process.env.YANDEX_MAIL_PASSWORD
+      }
+    };
+
+    /**
+     * Init transporter with date from config
+     */
+    this.transporter = nodemailer.createTransport(this.config);
+  }
+
+  /**
+   * Send email to specified receiver from template
+   * @param {string} to - email's receivers
+   * @param {string} templateName - email's subject
+   * @param {Object} variables - template variables
+   */
+  async send(to, templateName, variables) {
+    if (!templateName) throw new Error('Email\'s template name must be specified');
+
+    const emailContent = this.render(templateName, variables);
+
+    const mailOptions = {
+      from: `"${process.env.YANDEX_SENDER_NAME}" <${process.env.YANDEX_SENDER_ADDRESS}>`, // sender address
+      to,
+      ...emailContent
+    };
+
+    return this.transporter.sendMail(mailOptions);
+  }
+}
+
+module.exports = YandexProvider;
