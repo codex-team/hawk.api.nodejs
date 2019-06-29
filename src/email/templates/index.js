7308bdff32b527628521743b67f10afeffb9d85e
Nikita Melnikov <nikmel2803@gmail.com>
2019-06-29 15:30:05 +0300

    write docs for template/index.js

--- src/email/templates/index.js
+++ src/email/templates/index.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 
 /**
- * @type {Object} - contains email content (subject, text, html) for each template
+ * @type {Object.<string, EmailContent>} - contains email content (subject, text, html) for each template
  */
-const templates = fs.readdirSync(__dirname)
-  .filter(name => !name.endsWith('.js'))
+const templates = fs.readdirSync(__dirname) // read templates directory
+  .filter(name => !name.endsWith('.js')) // leave only template folders
   .reduce((accumulator, templateName) => {
     const templateDir = `${__dirname}/${templateName}/`;
     const templateContent = fs.readdirSync(templateDir);
 
+    // go to each folder and find the template files
     const subjectFilename = templateContent.find(fileName => fileName.startsWith('subject'));
     const htmlFilename = templateContent.find(fileName => fileName.startsWith('html'));
     const textFilename = templateContent.find(fileName => fileName.startsWith('text'));
 
+    // write content of the template files to the object
     accumulator[templateName] = {
       subject: fs.readFileSync(templateDir + subjectFilename).toString(),
       html: fs.readFileSync(templateDir + htmlFilename).toString(),
