bdcef56c42578402c7dcc4dbdd3e92cb4c57d396
Nikita Melnikov <nikmel2803@gmail.com>
2019-07-24 18:09:46 +0300

    fix auth directive for passing subscriptions

--- src/directives/requireAuthDirective.js
+++ src/directives/requireAuthDirective.js
@@ -18,7 +18,10 @@ class RequireAuthDirective extends SchemaDirectiveVisitor {
     /**
      * @type {GraphQLFieldResolver<*, *>} field resolver
      */
-    const { resolve = defaultFieldResolver } = field;
+    const {
+      resolve = defaultFieldResolver,
+      subscribe
+    } = field;
 
     /**
      * New field resolver
@@ -27,18 +30,33 @@ class RequireAuthDirective extends SchemaDirectiveVisitor {
     field.resolve = async function (...resolverArgs) {
       const [, , context] = resolverArgs;
 
-      if (context.user && context.user.accessTokenExpired) {
-        throw new AccessTokenExpiredError();
-      }
+      RequireAuthDirective.checkUser(context);
 
-      if (context.user && !context.user.id) {
-        throw new AuthenticationError(
-          'You must be signed in to view this resource.'
-        );
-      } else {
-        return resolve.apply(this, resolverArgs);
-      }
+      return resolve.apply(this, resolverArgs);
     };
+
+    field.subscribe = async function (...resolverArgs) {
+      const [, , context] = resolverArgs;
+
+      RequireAuthDirective.checkUser(context);
+      return subscribe.apply(this, resolverArgs);
+    };
+  }
+
+  /**
+   * Authorizes the user or throws an error if the data is incorrect
+   * @param {Context} context - request context
+   */
+  static checkUser(context) {
+    if (context.user && context.user.accessTokenExpired) {
+      throw new AccessTokenExpiredError();
+    }
+
+    if (context.user && !context.user.id) {
+      throw new AuthenticationError(
+        'You must be signed in to view this resource.'
+      );
+    }
   }
 }
 
