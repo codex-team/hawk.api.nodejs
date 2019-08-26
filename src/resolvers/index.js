4e33d115e130aed45ec23a4c08e2b916d5be9711
Danil Beltyukov <beltyukovdanil@gmail.com>
2019-08-26 16:58:03 +0300

    enable notify resolver

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -5,6 +5,7 @@ const user = require('./user');
 const workspace = require('./workspace');
 const project = require('./project');
 const event = require('./event');
+const notify = require('./notify');
 
 /**
  * @typedef ResolverObj
@@ -41,4 +42,4 @@ const indexResolver = {
   JSONObject: GraphQLJSONObject
 };
 
-module.exports = merge(indexResolver, user, workspace, project, event);
+module.exports = merge(indexResolver, user, workspace, project, event, notify);
