cc787c19894dbba61f5e9335d309178f45b3f8d4
Danil Beltyukov <beltyukovdanil@gmail.com>
2019-08-26 16:55:18 +0300

    notify models

---
+++ src/models/notify.js
@@ -0,0 +1,59 @@
+/**
+ * @typedef {Object} ProviderSettings
+ * @property {Boolean} enabled - provider enabled?
+ * @property {String} value - provider hook/email/etc
+ */
+
+/**
+ * @typedef {Object} NotifySettings
+ * @property {ProviderSettings} email
+ * @property {ProviderSettings} tg
+ * @property {ProviderSettings} slack
+ */
+
+/**
+ * @typedef {Object} NotifySchema
+ * @property {ObjectID} id - notify ID
+ * @property {ObjectID} projectId - project ID
+ * @property {NotifySettings} settings - notify settings
+ */
+
+/**
+ * Notify model
+ * Represents notify setting for given user and project
+ */
+class Notify {
+  /**
+   * Creates Notify instance
+   * @param {NotifySchema} schema - event's schema
+   */
+  constructor(schema = {}) {
+    if (schema) {
+      this.fillModel(schema);
+    }
+  }
+
+  /**
+   * @return {string|ObjectID}
+   */
+  get id() {
+    return this._id;
+  }
+
+  /**
+   * Fills current instance with schema properties
+   * @param {NotifySchema} schema
+   *
+   * @returns Event
+   */
+  fillModel(schema) {
+    for (const prop in schema) {
+      if (!schema.hasOwnProperty(prop)) {
+        continue;
+      }
+      this[prop] = schema[prop];
+    }
+  }
+}
+
+module.exports = Notify;
---
+++ src/models/notifyFactory.js
@@ -0,0 +1,151 @@
+const mongo = require('../mongo');
+const Notify = require('./notify');
+const { ObjectID } = require('mongodb');
+
+// const _ = require('lodash');
+
+/**
+ * NotifyFactory
+ *
+ * Creational class for Notify's Model
+ */
+class NotifyFactory {
+  /**
+   * Creates NotifyFactory instance
+   * @param {string|ObjectID} userId
+   */
+  constructor(userId) {
+    if (!userId) {
+      throw new Error('Can not construct Notify model, because userId is not provided');
+    }
+    this.userId = userId;
+
+    this.collection = mongo.databases.hawk.collection('notifies:' + this.userId);
+  }
+
+  /**
+   * Find Notify by passed query
+   *
+   * @param {object} [query={}] - query
+   * @param {Number} [limit=10] - query limit
+   * @param {Number} [skip=0] - query skip
+   * @returns {Promise<Notify[]>}
+   */
+  async find(query = {}, limit = 10, skip = 0) {
+    limit = this.validateLimit(limit);
+    skip = this.validateSkip(skip);
+
+    const cursor = this.collection
+      .find(query)
+      .limit(limit)
+      .skip(skip);
+
+    const result = await cursor.toArray();
+
+    return result.map(data => {
+      return new Notify(data);
+    });
+  }
+
+  /**
+   * Find Notify by project ID
+   *
+   * @param projectId
+   * @returns {Promise<Notify>}
+   */
+  async findByProjectId(projectId) {
+    const searchResult = await this.collection.findOne({ projectId: new ObjectID(projectId) });
+
+    return new Notify(searchResult);
+  }
+
+  /**
+   * Converts nested objects to one objects with properties as paths to nested props
+   * Example:
+   *
+   * objectToPath({projectId: ObjectID("5d63c84aa17ed33e62ed2edb"), settings: {email: {enabled: true}}})
+   *
+   * // { projectId: 5d63c84aa17ed33e62ed2edb, 'settings.email.enabled': true }
+   * @param obj - Object
+   * @param [curr] - current property
+   * @param [dict] - result
+   */
+  static propsToPaths(obj, curr = null, dict = {}) {
+    Object.keys(obj).forEach((key) => {
+      if (typeof (obj[key]) === 'object' && !(obj[key] instanceof ObjectID)) {
+        this.propsToPaths(obj[key], curr ? `${curr}.${key}` : key, dict);
+      } else {
+        dict[curr ? `${curr}.${key}` : key] = obj[key];
+      }
+    });
+    return dict;
+  }
+
+  /**
+   * Update Notify
+   *
+   * @param {Notify} notify - Notify to update
+   * @returns {Promise<Notfy|null>}
+   */
+  async update(notify) {
+    if (!notify.projectId) {
+      throw new Error('Can not update Notify, because projectId is not provided');
+    }
+
+    const updated = await this.collection.updateOne({ projectId: notify.projectId }, { $set: NotifyFactory.propsToPaths(notify) });
+
+    if (updated.matchedCount === 0) {
+      return null;
+    }
+
+    return true;
+  }
+
+  /**
+   * Create Notify
+   *
+   * @param {ObjectID} projectId - Project ID
+   * @param {NotifySettings} settings - Notify settings
+   * @returns {Promise<Notify>}
+   */
+  async create(projectId, settings = {}) {
+    if (!projectId) {
+      throw new Error('Can not create Notify, because projectId is not provided');
+    }
+    const inserted = await this.collection.insertOne({ projectId, settings });
+
+    return new Notify({ _id: inserted.insertedId, projectId, settings });
+  }
+
+  /**
+   * Validates limit value
+   * @param limit
+   * @return {Number}
+   */
+  validateLimit(limit) {
+    limit = Math.max(0, limit);
+
+    if (limit > 100) {
+      throw Error('Invalid limit value');
+    }
+
+    return limit;
+  }
+
+  /**
+   * Validate skip value
+   * @param skip
+   * @return {Number}
+   */
+  validateSkip(skip) {
+    skip = Math.max(0, skip);
+
+    if (skip > 100) {
+      throw Error('Invalid skip value');
+    }
+
+    return skip;
+  }
+}
+
+module.exports = NotifyFactory;
