2320ac7e36d66b2e1c8148abaa0ddcf035d2bdaf
Alexander Menshikov <n0str@users.noreply.github.com>
2019-09-01 02:02:00 +0300

    AMQP and MongoDB reconnect (#89)
    * Add mongodb reconnect options
    
    * Mongo reconnect
    
    * Add amqp reconnection
    
    * Fix double channel opening

--- src/rabbitmq.js
+++ src/rabbitmq.js
@@ -1,7 +1,8 @@
-const amqplib = require('amqplib');
-
+const amqplib = require('amqp-connection-manager');
+const debug = require('debug');
 const rabbitMQURL = process.env.AMQP_URL;
 let channel = null;
+let connection = null;
 
 /**
  * Setups connection to the RabbitMQ
@@ -9,11 +10,18 @@ let channel = null;
  */
 async function setupConnections() {
   if (rabbitMQURL) {
-    return amqplib.connect(rabbitMQURL)
-      .then(conn => conn.createChannel())
-      .then(ch => {
-        channel = ch;
-      });
+    connection = amqplib.connect(rabbitMQURL);
+    connection.on('connect', () => {
+      if (!channel) {
+        const channelWrapper = connection.createChannel({
+          setup: ch => {
+            channel = channelWrapper;
+            console.log(`🔗 AMQP channel connected: ${rabbitMQURL}`);
+          }
+        });
+      }
+    });
+    connection.on('disconnect', () => console.log('💥 AMQP disconnected. Trying to reconnect...'));
   }
   return null;
 }
@@ -27,9 +35,10 @@ async function setupConnections() {
  */
 async function publish(exchange, route, message) {
   try {
-    return channel.publish(exchange, route, Buffer.from(message));
-  } catch (e) {
-    console.log('RabbitMQ error:', e);
+    await channel.publish(exchange, route, Buffer.from(message));
+    debug(`Message sent: ${message}`);
+  } catch (err) {
+    console.log('Message was rejected:', err.stack);
   }
 }
 
