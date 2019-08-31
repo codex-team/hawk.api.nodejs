91f513483ce942abc3c2820f1a8017b8bb9c56d8
N0str <ntpcp@yandex.ru>
2019-09-01 00:12:43 +0300

    Add amqp reconnection

--- src/rabbitmq.js
+++ src/rabbitmq.js
@@ -1,7 +1,8 @@
-const amqplib = require('amqplib');
-
+const amqplib = require('amqp-connection-manager');
+const debug = require('debug');
 const rabbitMQURL = process.env.AMQP_URL;
 let channel = null;
+var connection = null;
 
 /**
  * Setups connection to the RabbitMQ
@@ -9,11 +10,16 @@ let channel = null;
  */
 async function setupConnections() {
   if (rabbitMQURL) {
-    return amqplib.connect(rabbitMQURL)
-      .then(conn => conn.createChannel())
-      .then(ch => {
-        channel = ch;
+    connection = amqplib.connect(rabbitMQURL);
+    connection.on('connect', () => {
+      var channelWrapper = connection.createChannel({
+        setup: ch => {
+          channel = channelWrapper;
+          console.log(`🔗 AMQP channel connected: ${rabbitMQURL}`);
+        }
       });
+    });
+    connection.on('disconnect', () => console.log('💥 AMQP disconnected. Trying to reconnect...'));
   }
   return null;
 }
@@ -26,11 +32,15 @@ async function setupConnections() {
  * @return {Promise<*>}
  */
 async function publish(exchange, route, message) {
-  try {
-    return channel.publish(exchange, route, Buffer.from(message));
-  } catch (e) {
-    console.log('RabbitMQ error:', e);
-  }
+  return channel.publish(exchange, route, Buffer.from(message))
+    .then(function () {
+      debug(`Message sent: ${message}`);
+    })
+    .catch(err => {
+      console.log('Message was rejected:', err.stack);
+      channel.close();
+      connection.close();
+    });
 }
 
 module.exports = {
