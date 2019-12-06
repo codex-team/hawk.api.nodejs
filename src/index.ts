2d8862d1ff9d68035e29be6f900418afd2b680ab
Nikita Melnikov <nikmel2803@gmail.com>
2019-12-06 04:05:53 +0300

    remove unused import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {ApolloServer} from 'apollo-server-express';
-import express, {Request} from 'express';
+import express from 'express';
 import mongo from './mongo';
 import rabbitmq from './rabbitmq';
 import jwt from 'jsonwebtoken';
