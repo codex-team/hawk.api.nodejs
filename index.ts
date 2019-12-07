f23b546cae5d3db1c5b8a90ff6c3f236fb12b4c2
Murod Khaydarov <murod.haydarov@gmail.com>
2019-12-07 17:51:38 +0300

    Setup improvements (#101)
    * Node setup improvements
    
    * update

--- index.ts
+++ index.ts
@@ -1,14 +1,9 @@
 import './src/env';
 import HawkAPI from './src';
 
-const start = async () => {
-  const app = new HawkAPI();
+const app = new HawkAPI();
 
-  try {
-    await app.start();
-  } catch (err) {
+app.start()
+  .catch(err => {
     console.log('Server runtime error' + err);
-  }
-};
-
-start();
+  });
