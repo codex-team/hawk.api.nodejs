5d65022638dd020f3cc112ba4947976d86d282a8
Nikita Melnikov <nikmel2803@gmail.com>
2019-12-12 13:13:36 +0300

    base factory and model

---
+++ src/models/abstactModelFactory.ts
@@ -0,0 +1,39 @@
+import {Collection, Db, ObjectID} from "mongodb";
+import BaseModel from './abstractModel'
+
+/**
+ * Model Factory class
+ */
+export default class Factory<DBScheme> {
+  private collection: Collection;
+  private readonly model: typeof BaseModel;
+
+  constructor(dbConnection: Db, collectionName: string, model: typeof BaseModel) {
+    this.collection = dbConnection.collection(collectionName);
+    this.model = model;
+  }
+
+  /**
+   * Find record by query
+   * @param query - query object
+   */
+  async findOne(query: object): Promise<BaseModel<DBScheme> | null> {
+    const searchResult = await this.collection.findOne(query);
+
+    if (!searchResult) return null;
+
+    return new this.model(searchResult);
+  }
+
+  /**
+   * Finds record by its id
+   * @param id - entity id
+   */
+  async findById(id: string): Promise<BaseModel<DBScheme> | null> {
+    const searchResult = await this.collection.findOne({
+      _id: new ObjectID(id)
+    });
+
+    return new this.model(searchResult);
+  }
+}
---
+++ src/models/abstractModel.ts
@@ -0,0 +1,32 @@
+import {Collection} from 'mongodb';
+
+/**
+ * Base model
+ */
+export default class Model<DBScheme> {
+  /**
+   * Creates model instance
+   * @param modelData - data to fill model
+   */
+  constructor(modelData: DBScheme) {
+  };
+
+
+  /**
+   * Model's collection
+   */
+  static get collection(): Collection {
+    throw new Error('Collection getter is not implemented');
+  }
+
+
+  /**
+   * Update entity data
+   * @param query - query to match
+   * @param data - update data
+   * @return number of documents modified
+   */
+  public static async update(query: object, data: object): Promise<number> {
+    return (await this.collection.updateOne(query, { $set: data })).modifiedCount;
+  }
+}
