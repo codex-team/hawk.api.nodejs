7b0653d012e1f281d94648c052d8891b29d638ce
Nikita Melnikov <nikmel2803@gmail.com>
2019-12-16 14:10:22 +0300

    Write new base model and factory (#108)
    * base factory and model
    
    * update docs
    
    * update docs and fix lint errors

---
+++ src/models/abstactModelFactory.ts
@@ -0,0 +1,58 @@
+import {Collection, Db, ObjectID} from "mongodb";
+import BaseModel from './abstractModel'
+
+/**
+ * Model Factory class
+ */
+export default class Factory<DBScheme> {
+  /**
+   * Collection to work with
+   */
+  private collection: Collection;
+
+  /**
+   * Model constructor to create instances
+   */
+  private readonly model: typeof BaseModel;
+
+  /**
+   * Creates factory instance
+   * @param dbConnection - connection to DataBase
+   * @param collectionName - database collection name
+   * @param model - model constructor
+   */
+  constructor(dbConnection: Db, collectionName: string, model: typeof BaseModel) {
+    this.collection = dbConnection.collection(collectionName);
+    this.model = model;
+  }
+
+  /**
+   * Find record by query
+   * @param query - query object
+   */
+  async findOne(query: object): Promise<BaseModel<DBScheme> | null> {
+    const searchResult = await this.collection.findOne(query);
+
+    if (!searchResult) {
+      return null;
+    }
+
+    return new this.model(searchResult);
+  }
+
+  /**
+   * Finds record by its id
+   * @param id - entity id
+   */
+  async findById(id: string): Promise<BaseModel<DBScheme> | null> {
+    const searchResult = await this.collection.findOne({
+      _id: new ObjectID(id)
+    });
+
+    if (!searchResult) {
+      return null;
+    }
+
+    return new this.model(searchResult);
+  }
+}
