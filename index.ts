f09719c67d543049727a033cc0770457d2db59e4
Taly <vitalik7tv@yandex.ru>
2020-06-11 20:34:29 +0300

    Add if

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import HawkAPI from './src';
 import HawkCatcher from '@hawk.so/nodejs';
 
 /** Enable HawkCatcher */
-HawkCatcher.init(process.env.HAWK_CATCHER_TOKEN || '');
+if (process.env.HAWK_CATCHER_TOKEN) {
+  HawkCatcher.init(process.env.HAWK_CATCHER_TOKEN);
+}
 
 const app = new HawkAPI();
 
