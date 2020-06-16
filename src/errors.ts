32904412fc9addfe4044c0e103291e691dffe13a
Nikita Melnikov <nikmel2803@gmail.com>
2020-06-16 19:46:38 +0300

    Update src/errors.ts
    Co-authored-by: Peter Savchenko <specc.dev@gmail.com>

--- src/errors.ts
+++ src/errors.ts
@@ -16,7 +16,7 @@ export const errorCodes = {
 };
 
 /**
- * Class for non critical errors (expected errors that we don't want to log)
+ * Class for non-critical errors (when the app works fine, but we need to send the error to the user, for example, auth-errors and others)
  * Events inherited from this class won't be send to hawk
  */
 export class NonCriticalError extends ApolloError {}
