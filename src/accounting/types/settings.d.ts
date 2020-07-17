865fb5c16db7576ad06f80a5ede16d32be463986
ilyamore88 <themorozik@gmail.com>
2020-07-17 13:53:31 +0300

    add docs

--- src/accounting/types/settings.d.ts
+++ src/accounting/types/settings.d.ts
@@ -1,7 +1,29 @@
+/**
+ * Settings for client module
+ */
 export interface Settings {
+  /**
+   * Base URL for sending queries
+   */
   baseURL: string;
+
+  /**
+   * Enable or disable tls verify
+   */
   tlsVerify: boolean;
+
+  /**
+   * Path to ca cert file
+   */
   tlsCaCertPath?: string;
+
+  /**
+   * Path to client cert file
+   */
   tlsCertPath?: string;
+
+  /**
+   * Path to client keys file
+   */
   tlsKeyPath?: string;
 }
