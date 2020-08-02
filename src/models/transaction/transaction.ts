007be4116d5887e25ca0997ef6fa34227d90b923
ilyamore88 <themorozik@gmail.com>
2020-08-02 16:10:04 +0300

    create transaction model

---
+++ src/models/transaction/transaction.ts
@@ -0,0 +1,99 @@
+import AbstractModel from '../abstractModel';
+import { Collection, ObjectId } from 'mongodb';
+
+/**
+ * Structure represents a Transaction in DataBase
+ */
+export interface TransactionDBScheme {
+  /**
+   * Transaction ID
+   */
+  _id: ObjectId;
+
+  /**
+   * Workspace ID to which the payment is credited
+   */
+  workspaceId: ObjectId;
+
+  /**
+   * Amount of payment
+   */
+  amount: number;
+
+  /**
+   * Transaction creation datetime
+   */
+  dtCreated: number;
+
+  /**
+   * Transaction status
+   */
+  status: string;
+
+  /**
+   * ID of the user who made the payment
+   */
+  userId: ObjectId;
+
+  /**
+   * PAN of card which user made the payment
+   */
+  cardPan: string;
+}
+
+/**
+ * Model representing transaction object
+ */
+export default class TransactionModel extends AbstractModel<TransactionDBScheme> implements TransactionDBScheme {
+  /**
+   * Transaction ID
+   */
+  public _id!: ObjectId;
+
+  /**
+   * Workspace ID to which the payment is credited
+   */
+  public workspaceId!: ObjectId;
+
+  /**
+   * Amount of payment
+   */
+  public amount!: number;
+
+  /**
+   * Transaction creation datetime
+   */
+  public dtCreated!: number;
+
+  /**
+   * Transaction status
+   */
+  public status!: string;
+
+  /**
+   * ID of the user who made the payment
+   */
+  public userId!: ObjectId;
+
+  /**
+   * PAN of card which user made the payment
+   */
+  public cardPan!: string;
+
+  /**
+   * Model's collection
+   *
+   * @protected
+   */
+  protected collection: Collection<TransactionDBScheme>;
+
+  /**
+   * Create Transaction instance
+   *
+   * @param transactionData - transaction data
+   */
+  constructor(transactionData: TransactionDBScheme) {
+    super(transactionData);
+    this.collection = this.dbConnection.collection<TransactionDBScheme>('transactions');
+  }
+}
