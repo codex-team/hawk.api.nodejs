969ca75e50325c0f291a2bcd23ee2675c8312e99
Taly <vitalik7tv@yandex.ru>
2020-08-03 19:09:15 +0300

    Update migrations

--- migrations/20200803141743-add-plans.js
+++ migrations/20200803141743-add-plans.js
@@ -1,10 +1,10 @@
 /**
- * @file Initialize plans collection
+ * @file Initialize plans collection and set default plan for each workspace
  */
 module.exports = {
-  async up(db) {
+  async up(db, client) {
     /**
-     * Define plans
+     * Define initial plans
      * @type {{isDefault: boolean, eventsLimit: number, monthlyCharge: number, name: string}[]}
      */
     const plans = [
@@ -29,17 +29,128 @@ module.exports = {
     ]
 
     /**
-     * Insert plans to DB
+     * Use one transaction for all requests
      */
-    await db.collection('plans').insertMany(plans);
+    const session = client.startSession();
+
+    console.log('Start to create plans and set default plans for existing workspaces...');
+
+    try {
+      await session.withTransaction(async () => {
+        /**
+         * Insert plans to DB
+         */
+        await db.collection('plans').insertMany(plans);
+
+        /**
+         * Get default plan data
+         * @type {PlanDBScheme}
+         */
+        const defaultPlan = await db.collection('plans').findOne({
+          isDefault: true
+        });
+
+        /**
+         * Get all workspaces
+         */
+        const workspaces = await db.collection('workspaces').find().toArray();
+
+        /**
+         * Each workspace should have a plan
+         */
+        await Promise.all(workspaces.map(async workspace => {
+          const workspaceId = workspace._id;
+
+          /**
+           * Skip workspaces, which already have plan
+           */
+          if (workspace.plan) {
+            console.log(`Workspace ${workspaceId} already have plan. Skipped.`);
+
+            return Promise.resolve();
+          }
+
+          /**
+           * Set up default plan
+           */
+          const result = await db.collection('workspaces').findOneAndUpdate(
+            {
+              _id: workspace._id,
+            },
+            {
+              $set: { plan: defaultPlan._id.toString() },
+            }
+          );
+
+          if (result.ok === 1) {
+            console.log(`Workspace ${workspaceId} updated`);
+          } else {
+            console.log(`Workspace ${workspaceId} failed updating`);
+          }
+
+          return result;
+        }));
+      });
+    } finally {
+      await session.endSession();
+    }
 
     return true;
   },
 
-  async down(db) {
+  async down(db, client) {
     /**
-     * Drop plans collection
+     * Use one transaction for all requests
      */
-    await db.collection('plans').drop();
+    const session = client.startSession();
+
+    console.log('Start to rollback users notifications config updating...');
+
+    try {
+      await session.withTransaction(async () => {
+        /**
+         * Drop plans collection
+         */
+        await db.collection('plans').drop();
+
+        /**
+         * Find workspaces with a plan
+         */
+        const workspaces = await db.collection('workspaces').find({
+          plan: {
+            $exists: true
+          }
+        }).toArray();
+
+        /**
+         * Remove plans from workspaces
+         */
+        await Promise.all(workspaces.map(async workspace => {
+          const workspaceId = workspace._id;
+
+          /**
+           * Set up default plan
+           */
+          const result = await db.collection('workspaces').findOneAndUpdate(
+            {
+              _id: workspace._id,
+            },
+            {
+              $unset: { plan: '' },
+            }
+          );
+
+          if (result.ok === 1) {
+            console.log(`Workspace ${workspaceId} now has no plan`);
+          } else {
+            console.log(`Workspace ${workspaceId} failed to remove a plan`);
+          }
+
+          return result;
+        }));
+      });
+    } finally {
+      await session.endSession();
+    }
   }
 };
