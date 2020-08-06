ff00b39e36f9aa53dae94dc8ad0ed2c5079d3363
Peter Savchenko <specc.dev@gmail.com>
2020-08-06 18:00:49 +0300

    Update billingNew.ts

--- src/resolvers/billingNew.ts
+++ src/resolvers/billingNew.ts
@@ -1,5 +1,12 @@
-import BusinessOperationModel from '../models/businessOperation';
+import BusinessOperationModel, {
+  BusinessOperationPayloadType,
+  PayloadOfDepositByUser,
+  PayloadOfWorkspacePlanPurchase
+} from '../models/businessOperation';
 import { ResolverContextWithUser } from '../types/graphql';
+import WorkspaceModel from '../models/workspace';
+import UserModel from '../models/user';
+// import { ObjectId } from 'mongodb';
 
 export default {
   Query: {
@@ -16,6 +23,91 @@ export default {
       { user, factories }: ResolverContextWithUser
     ): Promise<BusinessOperationModel[]> {
       return factories.businessOperationsFactory.getWorkspacesBusinessOperations(ids);
+
+      /**
+       * Leave for testing, will be removed when Garage-side will be finished
+       */
+      /*
+       * return [
+       *   {
+       *     id: '111',
+       *     type: 'WORKSPACE_PLAN_PURCHASE',
+       *     status: 'CONFIRMED',
+       *     transactionId: '123',
+       *     payload: {
+       *       workspaceId: new ObjectId('5e5fb6303e3a9d0a1933739a'),
+       *       amount: 1000,
+       *     },
+       *   } as unknown as BusinessOperationModel,
+       * ];
+       */
+    },
+  },
+  /**
+   * Resolver for Union Payload type.
+   * Represents two types of payload depending on the operation's type
+   */
+  BusinessOperationPayload: {
+    /**
+     * Returns type of the payload
+     * @param payload - result from resolver above
+     */
+    __resolveType(payload: BusinessOperationPayloadType): string {
+      if ('userId' in payload) {
+        return 'PayloadOfDepositByUser';
+      }
+
+      return 'PayloadOfWorkspacePlanPurchase';
+
+      /**
+       * @todo access to operation.type
+       */
+      /*
+       * if (operation.type) {
+       *   case BusinessOperationType.WorkspacePlanPurchase:
+       *     return 'PayloadOfWorkspacePlanPurchase';
+       *   default:
+       *   case BusinessOperationType.DepositByUser:
+       *     return 'PayloadOfDepositByUser';
+       * }
+       */
+    },
+  },
+
+  PayloadOfWorkspacePlanPurchase: {
+    /**
+     * Resolver for workspace by workspaceId
+     *
+     * @param payload - operation metadata
+     * @param _args - resolver args
+     * @param factories - resolver factories
+     */
+    async workspace(payload: PayloadOfWorkspacePlanPurchase, _args: undefined, { factories }: ResolverContextWithUser): Promise<WorkspaceModel | null> {
+      return factories.workspacesFactory.findById(payload.workspaceId.toHexString());
+    },
+  },
+
+  PayloadOfDepositByUser: {
+    /**
+     * Resolver for workspace by workspaceId
+     *
+     * @param payload - operation metadata
+     * @param _args - resolver args
+     * @param factories - resolver factories
+     */
+    workspace(payload: PayloadOfDepositByUser, _args: undefined, { factories }: ResolverContextWithUser): Promise<WorkspaceModel | null> {
+      return factories.workspacesFactory.findById(payload.workspaceId.toHexString());
+    },
+
+    /**
+     * Resolver for user by userId
+     *
+     * @param payload - operation metadata
+     * @param _args - resolver args
+     * @param factories - resolver factories
+     */
+    user(payload: PayloadOfDepositByUser, _args: undefined, { factories }: ResolverContextWithUser): Promise<UserModel | null> {
+      return factories.usersFactory.findById(payload.userId.toHexString());
     },
   },
 };
