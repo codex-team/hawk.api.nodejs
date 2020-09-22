7b68f60ceb8da8760c2b208a83c07ecc67095ee2
GeekaN <geekan@bk.ru>
2020-09-22 15:10:28 +0300

    Resolve conflicts

--- src/accounting/types/account.d.ts
+++ src/accounting/types/account.d.ts
@@ -2,23 +2,23 @@
  * The base unit of an accounting system
  */
 export interface Account {
-    /**
-     * Account id
-     */
-    id: string;
+  /**
+   * Account id
+   */
+  id: string;
 
-    /**
-     * Account name (for example, "Cashbook")
-     */
-    name?: string;
+  /**
+   * Account name (for example, "Cashbook")
+   */
+  name?: string;
 
-    /**
-     * Account currency
-     */
-    currency?: string;
+  /**
+   * Account currency
+   */
+  currency?: string;
 
-    /**
-     * Account balance
-     */
-    balance?: number;
-  }
+  /**
+   * Account balance
+   */
+  balance?: number;
+}
