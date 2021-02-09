83438756b55d6878d12c1184b6a7e62b95eb0ec6
Nikita Melnikov <nikmel2803@gmail.com>
2021-02-09 19:55:19 +0300

    add docs

--- test/integration/utils.ts
+++ test/integration/utils.ts
@@ -1,8 +1,14 @@
 import dotenv from 'dotenv';
 import axios from 'axios';
 
+/**
+ * Env variables for API
+ */
 export const apiEnv = dotenv.config({ path: './api.env' }).parsed || {};
 
+/**
+ * Axios instance to send requests to API
+ */
 export const apiInstance = axios.create({
   baseURL: `http://api:${apiEnv.PORT}`,
 });
