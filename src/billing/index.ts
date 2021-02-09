b7515ee9eacf2629765aa1447d04732983c020db
Murod Khaydarov <murod.haydarov@gmail.com>
2021-02-09 18:03:52 +0300

    use default cors settings

--- src/billing/index.ts
+++ src/billing/index.ts
@@ -19,30 +19,7 @@ export default class Billing {
   constructor(app: express.Application) {
     this.providerWebhooks = new CloudPaymentsWebhooks();
 
-    app.use(cors(this.corsOptionDelegate));
+    app.use(cors());
     app.use('/billing', this.providerWebhooks.getRouter());
-  }
-
-  /**
-   * Enables or Disables request on origin
-   *
-   * @param req - Express request object
-   * @param callback — Function that enables request execution on origin (uses special allowed list)
-   */
-  private corsOptionDelegate(req: express.Request, callback: Function): void {
-    const allowList = [
-      'http://localhost:8080',
-      'https://hawk.so',
-      'https://beta.hawk.so',
-      'https://stage.beta.hawk.so',
-    ];
-
-    const origin = req.header('Origin') || '';
-
-    if (allowList.indexOf(origin) !== -1) {
-      callback(null, { origin: true });
-    } else {
-      callback(null, { origin: false });
-    }
   }
 }
