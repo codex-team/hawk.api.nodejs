48c273809cf27250e8c2b40150ea4e81f0de36b8
Nikita Melnikov <nikmel2803@gmail.com>
2021-02-13 17:46:33 +0300

    create custom env for testing

---
+++ test/integration/jestEnv.js
@@ -0,0 +1,30 @@
+const NodeEnvironment = require('jest-environment-node');
+const mongodb = require('mongodb');
+
+/**
+ * Custom test environment for defining global connections
+ */
+class CustomEnvironment extends NodeEnvironment {
+  /**
+   * Setup environment
+   * @return {Promise<void>}
+   */
+  async setup() {
+    await super.setup();
+    const mongoClient = new mongodb.MongoClient('mongodb://mongodb:27017', { useUnifiedTopology: true });
+
+    await mongoClient.connect();
+    this.global.mongoClient = mongoClient;
+  }
+
+  /**
+   * Teardown environment
+   * @return {Promise<void>}
+   */
+  async teardown() {
+    await this.global.mongoClient.close();
+    await super.teardown();
+  }
+}
+
+module.exports = CustomEnvironment;
