2f5bb85334d7f474f38c3d44be3667b3511807c4
N0str <ntpcp@yandex.ru>
2021-06-17 12:15:09 +0300

    Add bulk write

--- migrations/20210617063327-unhardcode-image.js
+++ migrations/20210617063327-unhardcode-image.js
@@ -6,50 +6,62 @@ require('dotenv').config({
   path: '../.env',
 });
  
-function removePrefix(collection) {
-  collection.forEach(function( dcmt ) {
-    newImg = dcmt.image.replace(/https?:\/\/[a-zA-Z0-9.]+\/uploads\//, "/uploads/");
-    projects.updateOne(
-      { _id: dcmt._id }, 
-      { "$set": { "image": newImg } }
-    );
-  });
-}
-
-function addPrefixBack(collection) {
-  collection.forEach(function( dcmt ) {
-    newImg = process.env.API_URL + dcmt.image;
-    projects.updateOne(
-      { _id: dcmt._id }, 
-      { "$set": { "image": newImg } }
-    );
-  });
-}
- 
 module.exports = {
-  async up(db) {
+  async up(db, client) {
+    async function removePrefix(collection) {
+      const documents = await collection.find({image:{$ne: null}}).toArray();
+      const operations = documents.map(dcmt => {
+        newImg = dcmt.image.replace(/https?:\/\/[a-zA-Z0-9.]+\/uploads\//, "/uploads/");
+        return {
+          updateOne: {
+            filter: {
+              _id: dcmt._id
+            },
+            update: {
+              $set: {
+                "image": newImg
+              }
+            }
+          }
+        }
+      });
+      await collection.bulkWrite(operations);
+    }
     /**
      * Images are hardcoded in projects, users and workspaces
      */
-    var projects = db.collection('projects').find({image:{$ne: null}});
-    var users = db.collection('users').find({image:{$ne: null}});
-    var workspaces = db.collection('workspaces').find({image:{$ne: null}});
-    
-    removePrefix(projects);
-    removePrefix(users);
-    removePrefix(workspaces);
+    await removePrefix(db.collection('projects'));
+    await removePrefix(db.collection('users'));
+    await removePrefix(db.collection('workspaces'));
   },
 
   async down(db) {
+    async function addPrefixBack(collection) {
+      const documents = await collection.find({image:{$ne: null}}).toArray();
+      const operations = documents.map(dcmt => {
+        if (!dcmt.image.match(/https?:\/\/[a-zA-Z0-9.]+\/uploads\/\S+/)) {
+          dcmt.image = process.env.API_URL + dcmt.image;
+        }
+        return {
+          updateOne: {
+            filter: {
+              _id: dcmt._id
+            },
+            update: {
+              $set: {
+                "image": dcmt.image
+              }
+            }
+          }
+        }
+      });
+      await collection.bulkWrite(operations);
+    }
     /**
      * Images are hardcoded in projects, users and workspaces
      */
-    var projects = db.collection('projects').find({image:{$ne: null}});
-    var users = db.collection('users').find({image:{$ne: null}});
-    var workspaces = db.collection('workspaces').find({image:{$ne: null}});
-    
-    addPrefixBack(projects);
-    addPrefixBack(users);
-    addPrefixBack(workspaces);
+    await addPrefixBack(db.collection('projects'));
+    await addPrefixBack(db.collection('users'));
+    await addPrefixBack(db.collection('workspaces'));
   },
 };
