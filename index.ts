70a8c5e3f05dd641af6aec9cfe3010c986e90565
Nikita Melnikov <nikmel2803@gmail.com>
2022-08-05 11:33:51 +0400

    Setup ci (#425)
    * init typescript and server
    
    * add yarn cache for zero-install
    
    * add basic dockerfile
    
    * install and configure eslint
    
    * add ci for building docker image
    
    * add job for code linting
    
    * change lint command
    
    * add --max-warnings=0

--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
-import Fastify from 'fastify'
+import Fastify from 'fastify';
 const fastify = Fastify({
-    logger: true
-})
+  logger: true,
+});
 
 fastify.get('/', function (_request, reply) {
-    reply.send({ hello: 'world' })
-})
+  reply.send({ hello: 'world' });
+});
 
 // Run the server!
-fastify.listen({ port: 3000, host: "0.0.0.0" }, function (err) {
-    if (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
-    // Server is now listening on ${address}
-})
+fastify.listen({ port: 3000,
+  host: '0.0.0.0' }, function (err) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  // Server is now listening on ${address}
+});
 
-console.log('hello!')
+console.log('hello!');
