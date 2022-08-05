d4330d1fc03b8438304b730568ddeb00b17c4d33
Nikita Melnikov <nikmel2803@gmail.com>
2022-08-05 13:57:59 +0800

    init typescript and server

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# api
+# hawk.api
---
+++ index.ts
@@ -0,0 +1,19 @@
+import Fastify from 'fastify'
+const fastify = Fastify({
+    logger: true
+})
+
+fastify.get('/', function (_request, reply) {
+    reply.send({ hello: 'world' })
+})
+
+// Run the server!
+fastify.listen({ port: 3000 }, function (err) {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+    // Server is now listening on ${address}
+})
+
+console.log('hello!')
