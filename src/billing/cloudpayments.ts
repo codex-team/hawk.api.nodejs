2a5bcf3f39ecade0c2f2b40444f1783b44920813
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2024-12-06 21:48:17 +0300

    Return removed stuff

--- src/billing/cloudpayments.ts
+++ src/billing/cloudpayments.ts
@@ -343,34 +343,42 @@ export default class CloudPaymentsWebhooks {
 
     // let accountId = workspace.accountId;
 
-    // try {
-    //   if (!workspace.accountId) {
-    //     accountId = (await context.accounting.createAccount({
-    //       name: `WORKSPACE:${workspace.name}`,
-    //       type: AccountType.LIABILITY,
-    //       currency: Currency.RUB,
-    //     })).recordId;
-    //     await workspace.setAccountId(accountId);
-    //   }
+    /*
+     * try {
+     *   if (!workspace.accountId) {
+     *     accountId = (await context.accounting.createAccount({
+     *       name: `WORKSPACE:${workspace.name}`,
+     *       type: AccountType.LIABILITY,
+     *       currency: Currency.RUB,
+     *     })).recordId;
+     *     await workspace.setAccountId(accountId);
+     *   }
+     */
 
-    //   await context.accounting.payOnce({
-    //     accountId: accountId,
-    //     amount: tariffPlan.monthlyCharge * PENNY_MULTIPLIER,
-    //     description: `Account replenishment to pay for the tariff plan with id ${tariffPlan._id}. CloudPayments transaction ID: ${body.TransactionId}`,
-    //   });
+    /*
+     *   await context.accounting.payOnce({
+     *     accountId: accountId,
+     *     amount: tariffPlan.monthlyCharge * PENNY_MULTIPLIER,
+     *     description: `Account replenishment to pay for the tariff plan with id ${tariffPlan._id}. CloudPayments transaction ID: ${body.TransactionId}`,
+     *   });
+     */
 
-    //   await context.accounting.purchase({
-    //     accountId,
-    //     amount: tariffPlan.monthlyCharge * PENNY_MULTIPLIER,
-    //     description: `Charging for tariff plan with id ${tariffPlan._id}. CloudPayments transaction ID: ${body.TransactionId}`,
-    //   });
-    // } catch (e) {
-    //   const error = e as Error;
+    /*
+     *   await context.accounting.purchase({
+     *     accountId,
+     *     amount: tariffPlan.monthlyCharge * PENNY_MULTIPLIER,
+     *     description: `Charging for tariff plan with id ${tariffPlan._id}. CloudPayments transaction ID: ${body.TransactionId}`,
+     *   });
+     * } catch (e) {
+     *   const error = e as Error;
+     */
 
     //   this.sendError(res, PayCodes.SUCCESS, `[Billing / Pay] Error while creating operations in accounting ${error.toString()}`, body);
 
-    //   return;
-    // }
+    /*
+     *   return;
+     * }
+     */
 
     try {
       await publish('cron-tasks', 'cron-tasks/limiter', JSON.stringify({
@@ -481,7 +489,6 @@ export default class CloudPaymentsWebhooks {
     let user;
 
     if (!data.workspaceId || !data.userId || !data.tariffPlanId) {
-
       this.sendError(res, FailCodes.SUCCESS, `[Billing / Fail] No workspace or user id or plan id in request body`, body);
 
       return;
