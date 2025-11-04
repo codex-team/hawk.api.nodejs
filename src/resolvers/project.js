f00b7e7d6e3f50181cd7d6466df64cd8e46eea43
slaveeks <chernyshev2001@icloud.com>
2025-11-05 02:20:14 +0300

    add constants

--- src/resolvers/project.js
+++ src/resolvers/project.js
@@ -14,6 +14,8 @@ const EVENTS_GROUP_HASH_INDEX_NAME = 'groupHashUnique';
 const REPETITIONS_GROUP_HASH_INDEX_NAME = 'groupHash_hashed';
 const REPETITIONS_USER_ID_INDEX_NAME = 'userId';
 const EVENTS_TIMESTAMP_INDEX_NAME = 'timestamp';
+const GROUPING_TIMESTAMP_INDEX_NAME = 'groupingTimestamp';
+const GROUPING_TIMESTAMP_AND_GROUP_HASH_INDEX_NAME = 'groupingTimestampAndGroupHash';
 const MAX_SEARCH_QUERY_LENGTH = 50;
 
 /**
@@ -106,14 +108,14 @@ module.exports = {
       await projectDailyEventsCollection.createIndex({
         groupingTimestamp: 1,
       }, {
-        name: 'groupingTimestamp',
+        name: GROUPING_TIMESTAMP_INDEX_NAME,
       });
 
       await projectDailyEventsCollection.createIndex({
         groupingTimestamp: 1,
         groupHash: 1,
       }, {
-        name: 'groupingTimestampAndGroupHash',
+        name: GROUPING_TIMESTAMP_AND_GROUP_HASH_INDEX_NAME,
       });
 
       await projectEventsCollection.createIndex({
