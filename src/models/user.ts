caf14a1be45e4535b55c59cc77b21cc0b5b77ab3
slaveeks <chernyshev2001@icloud.com>
2025-11-07 00:41:08 +0300

    fix(users): return all users workspaces if no ids

--- src/models/user.ts
+++ src/models/user.ts
@@ -370,8 +370,13 @@ export default class UserModel extends AbstractModel<UserDBScheme> implements Us
   public async getWorkspacesIds(ids: (string | ObjectId)[] = []): Promise<string[]> {
     const res = [];
 
+    if (ids.length === 0) {
+      return Object.keys(this.workspaces);
+    }
+
     for (const id of ids) {
       const workspaceId = id.toString();
+      console.log(workspaceId);
       const workspace = this.workspaces[workspaceId];
 
       if (workspace && workspace.isPending !== true) {
