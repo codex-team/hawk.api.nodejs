d4b910a212110943f74f973961db599a965b4e5b
GeekaN2 <geekan@bk.ru>
2025-11-08 01:43:36 +0300

    feat: add an index for payload.release & update pipeline

--- src/resolvers/project.js
+++ src/resolvers/project.js
@@ -14,6 +14,7 @@ const EVENTS_GROUP_HASH_INDEX_NAME = 'groupHashUnique';
 const REPETITIONS_GROUP_HASH_INDEX_NAME = 'groupHash_hashed';
 const REPETITIONS_USER_ID_INDEX_NAME = 'userId';
 const EVENTS_TIMESTAMP_INDEX_NAME = 'timestamp';
+const EVENTS_PAYLOAD_RELEASE_INDEX_NAME = 'payloadRelease';
 const GROUPING_TIMESTAMP_INDEX_NAME = 'groupingTimestamp';
 const GROUPING_TIMESTAMP_AND_LAST_REPETITION_TIME_AND_ID_INDEX_NAME = 'groupingTimestampAndLastRepetitionTimeAndId';
 const GROUPING_TIMESTAMP_AND_GROUP_HASH_INDEX_NAME = 'groupingTimestampAndGroupHash';
@@ -128,12 +129,17 @@ module.exports = {
       });
 
       await projectEventsCollection.createIndex({
-        groupHash: 1,
+        'payload.release': 1
       },
       {
-        unique: true,
-        name: EVENTS_GROUP_HASH_INDEX_NAME,
+        name: EVENTS_PAYLOAD_RELEASE_INDEX_NAME,
+        background: true,
+        sparse: true,
       });
+
+      projectEventsCollection.createIndex({
+        groupHash: 1,
+      })
 
       await projectRepetitionsEventsCollection.createIndex({
         groupHash: 'hashed',
@@ -503,14 +509,7 @@ module.exports = {
         { $match: { projectId: project._id.toString() } },
         {
           $project: {
-            release: {
-              $convert: {
-                input: '$release',
-                to: 'string',
-                onError: '',
-                onNull: '',
-              },
-            },
+            release: '$release',
             commitsCount: { $size: { $ifNull: ['$commits', [] ] } },
             filesCount: { $size: { $ifNull: ['$files', [] ] } },
             _releaseIdSec: { $floor: { $divide: [ { $toLong: { $toDate: '$_id' } }, 1000] } },
@@ -524,14 +523,7 @@ module.exports = {
               {
                 $match: {
                   $expr: {
-                    $eq: [ {
-                      $convert: {
-                        input: '$payload.release',
-                        to: 'string',
-                        onError: '',
-                        onNull: '',
-                      },
-                    }, '$$rel'],
+                    $eq: [ '$payload.release', '$$rel' ],
                   },
                 },
               },
@@ -594,7 +586,6 @@ module.exports = {
                 uploadDate: fileInfo.uploadDate,
               } ])
             );
-
             enrichedFiles = enrichedFiles.map((entry) => {
               const meta = metaById.get(String(entry._id));
 
