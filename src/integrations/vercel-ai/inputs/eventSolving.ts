ac48e5c21776922fa03a508343d358f892e7ee99
slaveeks <chernyshev2001@icloud.com>
2025-11-09 04:21:46 +0300

    improve prompts

--- src/integrations/vercel-ai/inputs/eventSolving.ts
+++ src/integrations/vercel-ai/inputs/eventSolving.ts
@@ -2,10 +2,4 @@ import { EventData, EventAddons } from '@hawk.so/types';
 
 export const eventSolvingInput = (payload: EventData<EventAddons>) => `
 Payload: ${JSON.stringify(payload)}
-
-Предоставь ответ в следующем формате:
-
-1. Описание проблемы
-2. Решение проблемы
-3. Описание того, как можно предотвратить подобную ошибку в будущем
 `;
